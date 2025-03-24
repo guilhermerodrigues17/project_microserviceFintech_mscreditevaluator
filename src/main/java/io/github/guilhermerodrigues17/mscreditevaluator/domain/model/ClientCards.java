92c81a77ba52e97bf48305d0f94b4940bc9a76f0
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-24 18:31:03 -0300

    feat: Criando model para ClientCards

---
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/domain/model/ClientCards.java
@@ -0,0 +1,13 @@
+package io.github.guilhermerodrigues17.mscreditevaluator.domain.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ClientCards {
+
+    private String name;
+    private String brand;
+    private BigDecimal creditLimitAvailable;
+}
