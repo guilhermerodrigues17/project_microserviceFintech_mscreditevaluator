10af04088331455a8e01527c5e65a94a538bbf38
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-24 17:29:49 -0300

    Initial commit

---
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/MscreditevaluatorApplication.java
@@ -0,0 +1,15 @@
+package io.github.guilhermerodrigues17.mscreditevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MscreditevaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscreditevaluatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/guilhermerodrigues17/mscreditevaluator/MscreditevaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermerodrigues17.mscreditevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscreditevaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
