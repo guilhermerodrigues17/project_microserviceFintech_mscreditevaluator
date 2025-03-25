9c6dac2c1980a1a04d4fd145ce1d6ccdef3e66c7
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-25 18:49:02 -0300

    feat: Implementado endpoint para client evaluation

--- src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/application/CreditEvaluatorController.java
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/application/CreditEvaluatorController.java
@@ -2,14 +2,13 @@ package io.github.guilhermerodrigues17.mscreditevaluator.application;
 
 import io.github.guilhermerodrigues17.mscreditevaluator.application.exceptions.ClientDataNotFoundException;
 import io.github.guilhermerodrigues17.mscreditevaluator.application.exceptions.MicroserviceCommsException;
+import io.github.guilhermerodrigues17.mscreditevaluator.domain.model.ClientEvaluationResponse;
 import io.github.guilhermerodrigues17.mscreditevaluator.domain.model.ClientSituation;
+import io.github.guilhermerodrigues17.mscreditevaluator.domain.model.EvaluationData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("credit-evaluator")
@@ -28,6 +27,18 @@ public class CreditEvaluatorController {
         try {
             ClientSituation clientSituation = creditEvaluatorService.getClientSituation(cpf);
             return ResponseEntity.ok(clientSituation);
+        } catch (ClientDataNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (MicroserviceCommsException e) {
+            return ResponseEntity.status(HttpStatus.valueOf(e.getStatus())).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> clientEvaluation(@RequestBody EvaluationData data) {
+        try {
+            ClientEvaluationResponse evaluation = creditEvaluatorService.clientEvaluation(data.getCpf(), data.getIncome());
+            return ResponseEntity.ok(evaluation);
         } catch (ClientDataNotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (MicroserviceCommsException e) {
