c15be5bd28a7993b2724c1b4d42180f52df9b665
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-28 19:23:28 -0300

    feat: Implementado bean de config pro rabbitmq

---
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/config/MQConfig.java
@@ -0,0 +1,16 @@
+package io.github.guilhermerodrigues17.mscreditevaluator.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfig {
+
+    @Value("${mq.queues.card-issuance}")
+    private String cardIssuanceQueue;
+
+    public Queue queueCardIssuance() {
+        return new Queue(cardIssuanceQueue, true);
+    }
+}
