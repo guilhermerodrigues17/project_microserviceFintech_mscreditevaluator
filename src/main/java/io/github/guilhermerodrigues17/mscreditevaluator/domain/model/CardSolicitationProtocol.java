f39c1b4ff544ee2550f0a3220d932f8189a2907d
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-04-11 13:00:49 -0300

    feat: implementada representação para o protocolo de solicitação

---
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/domain/model/CardSolicitationProtocol.java
@@ -0,0 +1,10 @@
+package io.github.guilhermerodrigues17.mscreditevaluator.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CardSolicitationProtocol {
+    private String protocol;
+}
