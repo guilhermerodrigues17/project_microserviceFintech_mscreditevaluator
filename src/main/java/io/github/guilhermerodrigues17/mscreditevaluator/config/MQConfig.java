83da48974302f5fb1aec786349adc14262d431e4
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-04-11 12:59:50 -0300

    fix: correção do bean

--- src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/config/MQConfig.java
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/config/MQConfig.java
@@ -2,6 +2,7 @@ package io.github.guilhermerodrigues17.mscreditevaluator.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,6 +11,7 @@ public class MQConfig {
     @Value("${mq.queues.card-issuance}")
     private String cardIssuanceQueue;
 
+    @Bean
     public Queue queueCardIssuance() {
         return new Queue(cardIssuanceQueue, true);
     }
