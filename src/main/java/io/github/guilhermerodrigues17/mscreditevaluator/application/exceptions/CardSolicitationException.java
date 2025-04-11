bb48ec4c813ed17e4f2eb8b987a2d640dfafbb6a
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-04-11 13:00:17 -0300

    feat: implementada exception para solicitação de cartões

---
+++ src/main/java/io/github/guilhermerodrigues17/mscreditevaluator/application/exceptions/CardSolicitationException.java
@@ -0,0 +1,7 @@
+package io.github.guilhermerodrigues17.mscreditevaluator.application.exceptions;
+
+public class CardSolicitationException extends RuntimeException {
+    public CardSolicitationException(String message) {
+        super(message);
+    }
+}
